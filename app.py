4b4d6a26454c215b5ec0c69ed68d7f9e7e13b9ea
KonstantinaStrantzali <konstrantz@gmail.com>
2022-03-26 20:57:07 +0000

    routing the pages using Jinja framework

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("welcome.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
