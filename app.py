3ddf48a456e9d8bd73043e60bc0095e95ce24aa3
KonstantinaStrantzali <konstrantz@gmail.com>
2022-03-28 12:19:43 +0000

    add functionality to login form

--- app.py
+++ app.py
@@ -45,15 +45,40 @@ def register():
         flash("Registration Successful!")
     return render_template("register.html")
 
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                    "trails", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
-@app.route("/trails")
-def trails():
-    return render_template("trails.html")
+@app.route("/trails<username>", methods=["GET", "POST"])
+def trails(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("trails.html", username=username)
+
 
 
 @app.route("/add_trail")
