ae413f43cf2e1591f1b9d2da791d3f8b02668c06
KonstantinaStrantzali <konstrantz@gmail.com>
2022-04-10 09:42:49 +0000

    set conditions for forms validation and different feedback and color display  depending the user's valid/invalid input

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,32 @@
 (function () {
-    'use strict'
-    const forms = document.querySelectorAll('.requires-validation')
-    Array.from(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          } 
-          
-    
-          form.classList.add('was-validated')
-        }, false)
-      })
-    })()
-
-    function checkInput() {
-        let value = document.getElementById("title").value;
-        console.log(value);
-        if ( value.length == 5 ) {
-            input.classList.remove("is-valid" );
-            input.classList.add( "is-invalid" );
-        } else {
-            input.classList.add("is-valid" );
-            input.classList.remove("is-invalid");
+  'use strict'
+  const forms = document.querySelectorAll('.requires-validation')
+  Array.from(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
         }
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+    function checkInput(input) {
+      //const input = document.getElementById("title")
+      const value = input.value;
+      
+      if (value.length < 5) {
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+      } 
+      else if (input.type == "password" && value.length < 8)  {
+        password.classList.remove("is-valid");
+        password.classList.add("is-invalid");
+      }
+      else {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+      }
     }
